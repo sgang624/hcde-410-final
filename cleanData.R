c566b6a709627e1173b37071986f68b57ad8a698
sgang624 <sgang624@gmail.com>
2023-06-04 22:35:25 -0700

    Update cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -3,15 +3,14 @@
 # Load HPI_CrimeRate.csv into a dataframe. Use dplyr to filter out empty rows
 # to make sure the data is clean.
 #
-# Please change the working directory function to the path where the local
+# Note: change the working directory function to the path where the local
 # repository is stored.
 #----------------------------------------------------------------------------#
 library(tidyverse)
-hpi_crime_raw <- read.csv("data/HPI_CrimeRate.csv")
+hpi_crime_raw <- read.csv("HPI_CrimeRate.csv")
 clean_data <- function(data){
   clean <- data %>% 
     filter(Year != "")
   return(clean)
 }
 hpi_crime_clean <- clean_data(hpi_crime_raw)
-?getwd
